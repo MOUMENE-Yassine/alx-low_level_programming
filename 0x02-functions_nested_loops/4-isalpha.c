03869d8bfaaa46bdeeeb3c632a37d880005a266b
MOUMENE-Yassine <moumene.yassineus@gmail.com>
2023-03-22 11:05:59 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _isalpha - check lowercases or uppercase lettres.
+*
+* @c: character to be checked
+*
+* Return: 1 for lowercase or uppercase or 0 for otherwise
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) ||( c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	return (0);
+}
