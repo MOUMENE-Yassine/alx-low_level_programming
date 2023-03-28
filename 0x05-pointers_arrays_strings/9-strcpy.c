5da2032ffc431e872e1fa839346ec1d38170d64a
MOUMENE-Yassine <moumene.yassineus@gmail.com>
2023-03-28 10:12:01 -0700

    9.strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* *_strcpy - unction that copies the string pointed.
+* @dest: distination string.
+* @src: source string.
+* Return: the pointer to dest.
+*/
+char *_strcpy(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (ptr);
+}
