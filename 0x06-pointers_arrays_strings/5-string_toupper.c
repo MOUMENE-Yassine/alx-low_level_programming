67bfcd6521182a6af879575b5427ce51de52383f
MOUMENE-Yassine <moumene.yassineus@gmail.com>
2023-03-30 19:01:58 -0700

    5.Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* *string_toupper - function that changes all lowercase letters.
+* @str: the string to be changed.
+* Return: the result.
+*/
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		if (str[index] >= 'a' && str[index] >= 'z')
+			str[index] -= 32;
+		index++;
+	}
+	return (str);
+}
