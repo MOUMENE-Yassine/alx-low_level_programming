a0ecea15afc0bdbba007126fea9e983ba2f0300c
MOUMENE-Yassine <moumene.yassineus@gmail.com>
2023-04-21 19:34:22 -0700

    3. To be is a to be the value of a variable.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,47 +5,45 @@
 
 /**
 * format_char - format character.
-* @separator: separator strings.
 * @ap: argument pointer.
 */
-void format_char(char *separator, va_list ap)
+void format_char(va_list ap)
 {
-	printf("%s%c", separator, va_arg(ap, int));
+	printf("%c", va_arg(ap, int));
 }
 
 /**
 * format_int - format integer.
-* @separator: separator strings.
 * @ap: argument pointer.
 */
-void format_int(char *separator, va_list ap)
+void format_int(va_list ap)
 {
-	printf("%s%d", separator, va_arg(ap, int));
+	printf("%d", va_arg(ap, int));
 }
 
 /**
 * format_float - format float.
-* @separator: separator strings.
 * @ap: argument pointer.
 */
-void format_float(char *separator, va_list ap)
+void format_float(va_list ap)
 {
-	printf("%s%f", separator, va_arg(ap, double));
+	printf("%f", va_arg(ap, double));
 }
 
 /**
 * format_string - format string.
-* @separator: separator strings.
 * @ap: argument pointer.
 */
-void format_string(char *separator, va_list ap)
+void format_string(va_list ap)
 {
 	char *s = va_arg(ap, char*);
 
-	switch ((int)(!s))
-	case 1:
-		s = "(nil)";
-		printf("%s%s", separator, s);
+	if (s == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+		printf("%s", s);
 }
 
 /**
@@ -53,11 +51,6 @@ void format_string(char *separator, va_list ap)
 * @format: format of strings.
 * Return: anything.
 */
-/
- typedef struct array {
-    char *array;
-    void (*f)(char *, va_list);
-} array_t;
 
 void print_all(const char * const format, ...)
 {
@@ -65,28 +58,30 @@ void print_all(const char * const format, ...)
 	char *separator = "";
 	va_list ap;
 
-	int array_t arrays[] = {
-		{"c", format_char},
-		{"i", format_int},
-		{"f", format_float},
-		{"s", format_string},
-		{NULL, NULL}
+	array_t arrays[] = {
+		{'c', format_char},
+		{'i', format_int},
+		{'f', format_float},
+		{'s', format_string},
+		{'\0', NULL}
 	};
 	va_start(ap, format);
 	while (format && format[i])
 	{
 		j = 0;
-		while (arrays[j].array)
+		while (arrays[j].letter != '\0')
 		{
-			if (format[i] == arrays[j].array[0])
+			if (format[i] == arrays[j].letter)
 			{
-				arrays[j].f(separator, ap);
+				printf("%s", separator);
+				arrays[j].f(ap);
 				separator = ", ";
+				break;
 			}
 			j++;
 		}
 		i++;
 	}
-	printf("\n");
 	va_end(ap);
+	printf("\n");
 }
